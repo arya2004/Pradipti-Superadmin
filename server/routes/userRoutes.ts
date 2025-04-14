d7c437338884ba013365a440f3ce74902d141dec
arya2004 <arya20j4@gmail.com>
2025-04-14 21:03:49 +0530

    Remove obsolete route files for institutions, internship programs, internship topics, MOUs, stations, students, and users to enhance codebase maintainability.

--- server/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUsers, createNewUser } from '../controllers/userController';
-
-const router = express.Router();
-router.use(express.json()); // Make sure this is before your routes
-router.get('/users', getUsers);
-router.post('/users', createNewUser);
-
-export default router;
